e3349fa427a2e83c4d576bd82dbc07ed93b3a525
Andi Tangguh <anditangguhf@gmail.com>
2020-11-04 01:37:09 +0700

    Hotfix

--- Login/db.php
+++ Login/db.php
@@ -9,7 +9,7 @@ $conn = new mysqli($serv, $unm, $pwd, $db_name);
 if ($conn->connect_error) {
   die("Connection failed: " . $conn->connect_error);
 } else {
-  $table_absensi = "_absensi";
+  $table_absensi = "absensi";
   $table_user = "user";
   $jam_masuk = date("Y-m-d 06:45:00");
   $jam_pulang = date("Y-m-d 15:00:00");
